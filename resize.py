3b7de0c0737f6641c442c68eb5ca59ed83128938
TomislavVacek <119018067+TomislavVacek@users.noreply.github.com>
2023-10-03 21:40:13 +0200

    Add files via upload

---
+++ resize.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import os, sys
+
+path = "Face Mask Dataset/Train/WithMask/"
+dirs = os.listdir( path )
+
+def resize():
+    for item in dirs:
+        if os.path.isfile(path+item):
+            im = Image.open(path+item)
+            f, e = os.path.splitext(path+item)
+            imResize = im.resize((224,224), Image.ANTIALIAS)
+            imResize.save(f + ' resized.png', 'PNG')
+resize()
